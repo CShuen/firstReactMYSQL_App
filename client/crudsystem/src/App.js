a26f32fc82349c76d2a1e069cd1ef8a0d721106e
LeeShuEn <cshuen0217@gmail.com>
2023-04-10 21:32:54 +0800

    prettier

--- client/crudsystem/src/App.js
+++ client/crudsystem/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Navbar from "./Navbar";
 
 function App() {
@@ -9,99 +9,123 @@ function App() {
   // const [updateName, setNewName] = useState("");
   const [updateDesc, setNewDesc] = useState("");
   const [onchangeId, setOnchangeId] = useState("");
-  
-  const [product, setProduct] = useState([])
-  const handleSubmit = () =>{
-    console.log("submitted",prodName)
-    axios.post("http://localhost:3001/api/add_product",{
-      prodName, desc
-    }).then((res)=>{
-      alert("Successfully Added Product")
-      getProdct()
-      setProdName("");
-      setDesc("")
-    }).catch((err)=>{
-      console.log(err)
-    })
-  }
 
-  const getProdct = () =>{
-    axios.get("http://localhost:3001/api/get_product")
-      .then((res)=>{
-        console.log(res.data)
-        let list = res.data
-        setProduct(list)
+  const [product, setProduct] = useState([]);
+  const handleSubmit = () => {
+    console.log("submitted", prodName);
+    axios
+      .post("http://localhost:3001/api/add_product", {
+        prodName,
+        desc,
       })
-      .catch((err)=>{
-        console.log(err)
+      .then((res) => {
+        alert("Successfully Added Product");
+        getProdct();
+        setProdName("");
+        setDesc("");
       })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-const deleteProduct = (id) =>{
-  axios.delete(`http://localhost:3001/api/delete_product/${id}`)
-      .then((res)=>{
-        alert("Deleted success")
-        getProdct()
-        
+  const getProdct = () => {
+    axios
+      .get("http://localhost:3001/api/get_product")
+      .then((res) => {
+        console.log(res.data);
+        let list = res.data;
+        setProduct(list);
       })
-      .catch((err)=>{
-        console.log(err)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteProduct = (id) => {
+    axios
+      .delete(`http://localhost:3001/api/delete_product/${id}`)
+      .then((res) => {
+        alert("Deleted success");
+        getProdct();
       })
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-const updateProduct = (id) =>{
-  console.log("updateDesc",updateDesc)
-  if(onchangeId !== id){
-    setNewDesc("")
-    alert("Please enter description")
-    return
-  }
-  axios.put(`http://localhost:3001/api/update_product/${id}`,{
-    description: updateDesc,
-  })
-  .then((res)=>{
-    
-    alert("Update success")
-    getProdct()
-    setNewDesc("")
-    
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
-}
+  const updateProduct = (id) => {
+    console.log("updateDesc", updateDesc);
+    if (onchangeId !== id) {
+      setNewDesc("");
+      alert("Please enter description");
+      return;
+    }
+    axios
+      .put(`http://localhost:3001/api/update_product/${id}`, {
+        description: updateDesc,
+      })
+      .then((res) => {
+        alert("Update success");
+        getProdct();
+        setNewDesc("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  useEffect(()=>{
-    getProdct()
-  },[])
+  useEffect(() => {
+    getProdct();
+  }, []);
   return (
     <div className="App">
-      
       <h1>First CRUD ReactJS + MYSQL App</h1>
       <label>Product name</label>
-      <input type={"text"} placeholder="Enter name" value={prodName} onChange={(e)=>setProdName(e.target.value)}/>
+      <input
+        type={"text"}
+        placeholder="Enter name"
+        value={prodName}
+        onChange={(e) => setProdName(e.target.value)}
+      />
       <label>Description</label>
-      <input type={"text"} placeholder="Describe the product..." value={desc} onChange={(e)=>setDesc(e.target.value)}/>
+      <input
+        type={"text"}
+        placeholder="Describe the product..."
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
       <button onClick={handleSubmit}>Add</button>
-      
+
       <div className="prod-container">
         <h2>Product List</h2>
-        {product && product.map((prod)=>{
-          return(
-            <div key={prod.id + prod.product_name} className="prod-row">
-              <div className="prod-name">
-                {prod.product_name}
+        {product &&
+          product.map((prod) => {
+            return (
+              <div key={prod.id + prod.product_name} className="prod-row">
+                <div className="prod-name">{prod.product_name}</div>
+
+                <div style={{ marginBottom: "30px" }}>{prod.description}</div>
+                <input
+                  type={"text"}
+                  placeholder="Updat Description"
+                  defaultValue={""}
+                  onClick={() => {
+                    setOnchangeId(prod.id);
+                  }}
+                  onChange={(e) => setNewDesc(e.target.value)}
+                />
+                <button onClick={() => updateProduct(prod.id)}>Update</button>
+                <button
+                  onClick={() => {
+                    deleteProduct(prod.id);
+                  }}
+                >
+                  Delete
+                </button>
               </div>
-              
-              <div style={{marginBottom:"30px"}}>
-                {prod.description}
-              </div>
-              <input type={"text"} placeholder="Updat Description" defaultValue={""} onClick={()=>{setOnchangeId(prod.id)}} onChange={(e)=>setNewDesc(e.target.value)}/>
-              <button onClick={()=>updateProduct(prod.id)}>Update</button>
-              <button onClick={()=>{deleteProduct(prod.id)}}>Delete</button>
-            </div>
-          )
-        })}
+            );
+          })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -8,50 +8,51 @@ const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "crudapp"
-})
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true}))
+  database: "crudapp",
+});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/add_product", (req, res) => {
   const prodName = req.body.prodName;
   const desc = req.body.desc;
-  console.log("add product: ", prodName)
-  const sqlInsert = "INSERT INTO backoffice (product_name, description) VALUES (?, ?)"
+  console.log("add product: ", prodName);
+  const sqlInsert =
+    "INSERT INTO backoffice (product_name, description) VALUES (?, ?)";
   // const select = "SELECT * FROM backoffice"
-  db.query(sqlInsert,[prodName, desc], (err, result)=>{
-    res.send("Successfully added product into backoffice table")
-  })  
+  db.query(sqlInsert, [prodName, desc], (err, result) => {
+    res.send("Successfully added product into backoffice table");
+  });
   // res.send("Success Called API - new");
 });
 
-app.get("/api/get_product", (req,res)=>{
-  const select = "SELECT * FROM backoffice"
-  db.query(select, (err, result)=>{
-    res.send(result)
-  })  
-})
+app.get("/api/get_product", (req, res) => {
+  const select = "SELECT * FROM backoffice";
+  db.query(select, (err, result) => {
+    res.send(result);
+  });
+});
 
-app.delete("/api/delete_product/:id", (req,res)=>{
-  const id = req.params.id
-  const sqlDelete = "DELETE from backoffice WHERE id = ?"
-  db.query(sqlDelete, [id], (err, result)=>{
-    if(err)console.log(err)
-    res.send(`Deleted (id: ${id})`)
-  })
-})
+app.delete("/api/delete_product/:id", (req, res) => {
+  const id = req.params.id;
+  const sqlDelete = "DELETE from backoffice WHERE id = ?";
+  db.query(sqlDelete, [id], (err, result) => {
+    if (err) console.log(err);
+    res.send(`Deleted (id: ${id})`);
+  });
+});
 
-app.put("/api/update_product/:id", (req, res)=>{
-  const id = req.params.id
+app.put("/api/update_product/:id", (req, res) => {
+  const id = req.params.id;
   const desc = req.body.description;
-  const sqlUpdate = "UPDATE backoffice SET description = ? WHERE id = ?"
+  const sqlUpdate = "UPDATE backoffice SET description = ? WHERE id = ?";
 
-  db.query(sqlUpdate, [desc,id], (err, result)=>{
-    if(err)console.log(err)
-    res.send(`Update success (id: ${id})`)
-  })
-})
+  db.query(sqlUpdate, [desc, id], (err, result) => {
+    if (err) console.log(err);
+    res.send(`Update success (id: ${id})`);
+  });
+});
 
 app.listen("3001", () => {
   console.log("Server is running");
